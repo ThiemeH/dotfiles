287d837e8423cbde6dafe71fe96fa732ee25eced
Thieme <ThiemeH@noreply.github.com>
2026-06-07 22:12:10 +0200

    Replaced gammastep with wlsunset

--- private_dot_config/home-manager/modules/desktop/niri.nix
+++ private_dot_config/home-manager/modules/desktop/niri.nix
@@ -5,10 +5,10 @@
   services.swaync.enable = true;
   services.swayidle.enable = true;
   services.awww.enable = true;
-  services.gammastep = {
+  services.wlsunset = {
     enable = true;
-    latitude = 52.37; # Amsterdam
-    longitude = 4.90;
+    latitude = "52.37"; # Amsterdam
+    longitude = "4.90";
     temperature.day = 6500;
     temperature.night = 1500;
   };
